e102d040c497f21b0388b9e97d187c821629bdd1
Ariane Mafra <arianecristinamafra@gmail.com>
2022-12-09 17:43:11 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -6,32 +6,31 @@ public class Main {
     private static Random random = new Random();
     static Scanner scanner = new Scanner(System.in);
     static final int tamanhoCartela = 5;
+    static final int qntSorteados = 5;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("#####Seja Bem-Vindo ao Super Bingo!#####\n");
-        System.out.println("Vamos cadastrar os jogaores dessa partida...\n");
-
+        System.out.println("Vamos cadastrar os jogadores dessa partida...\n");
         System.out.println("Entre com o nome dos jogadores separados por hífen e sem espaços. Exemplo: player1-player2-player3");
 
         String nomes = scanner.next();
         String[] nomesJogadores = nomes.split("-");
 
         int[][] cartelas = new int[nomesJogadores.length][tamanhoCartela];
+        int [] sorteados = new int [60];
+        for (int i = 0; i > 60; i++) {
+            sorteados[i] = 0;
+        }
 
         System.out.println("Selecione o modo de geração das cartelas.");
         manualOuAutomatico();
         int opGeracaoCartela = scanner.nextInt();
 
-        switch (opGeracaoCartela){
-            case 1:
-                gerarCartelaManual(nomesJogadores, cartelas);
-                break;
-            case 2:
-                gerarCartelaAutomatico(nomesJogadores, cartelas);
-                break;
-            default:
-                System.out.println("Opção inválida :( Tente novamente");
+        switch (opGeracaoCartela) {
+            case 1 -> gerarCartelaManual(nomesJogadores, cartelas);
+            case 2 -> gerarCartelaAutomatico(nomesJogadores, cartelas);
+            default -> System.out.println("Opção inválida :( Tente novamente");
         }
 
         System.out.println("Os Jogadores e as cartelas são:");
@@ -43,11 +42,15 @@ public class Main {
 
         switch (opGeracaoSorteio){
             case 1:
-                //TODO implementar geração manual do sorteio, consultar numeros iguais e pedir nova cartela
+                sorteioManual(sorteados);
                 //TODO marcar cartela
                 //TODO consultar se alguem fez bingo
                 break;
             case 2:
+                sorteioAutomatico(sorteados);
+                sorteioAutomatico(sorteados);
+
+
                 //TODO implementar geração automatica do sorteio, consultar numeros iguais
                 //TODO marcar cartela
                 //TODO consultar se alguem fez bingo
@@ -56,6 +59,63 @@ public class Main {
                 System.out.println("Opção inválida :( Tente novamente");
         }
     }
+
+    private static void sorteioAutomatico(int[] sorteados) {
+        int[] sorteio = new int[qntSorteados];
+
+        for (int i = 0; i <sorteio.length; i++) {
+            for (int k = 0; k < i; k++ ) {
+                sorteio[i] = random.nextInt(1, 60);
+                if (sorteio[i] == sorteio[k]) {
+                    i--;
+                }else {
+                    for (int j = 0; j < sorteados.length; j++) {
+                        if (sorteio[i] == sorteados[j]) {
+                            i--;
+                        } else {
+                            sorteados[sorteio[i] - 1] = sorteio[i];
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(sorteio));
+
+
+        System.out.println(Arrays.toString(sorteados));
+    }
+
+    private static void novoSorteioManual(int []sorteados) {
+        System.out.println("Deseja fazer um novo sorteio?");
+        System.out.println("1 - SIM");
+        System.out.println("2 - SAIR DO JOGO");
+        System.out.println("Digite o número da opção desejada.");
+
+        int opNovoSorteio = scanner.nextInt();
+        switch (opNovoSorteio) {
+            case 1 -> sorteioManual(sorteados);
+            case 2 -> System.out.println("Você saiu do jogo!");
+            default -> System.out.println("Opção inválida :( Tente novamente");
+        }
+    }
+    private static void sorteioManual(int [] sorteados) {
+        int [] sorteio = new int[qntSorteados];
+        System.out.printf("Você deve entrar com os %d números sortedos (entre 0 e 60), separados por vírgula e sem espaço." +
+                "\nExemplo: 20,26,52,40,2" +
+                "\nOBS.: Os números não devem se repetir.\n\n", qntSorteados);
+
+        String numeros = scanner.next();
+        String[] sorteadosManual = numeros.split(",");
+
+        for (int i = 0; i < sorteadosManual.length; i++) {
+            sorteio[i] = Integer.parseInt(sorteadosManual[i]);
+            sorteados[sorteio[i] - 1] = sorteio[i];
+        }
+
+        System.out.println(Arrays.toString(sorteados));
+        novoSorteioManual(sorteados);
+    }
+
     private static void mostrarJogadoresECartelas(String[] nomesJogadores, int[][] cartelas) {
         for (int i = 0; i <nomesJogadores.length; i++){
             System.out.print("Jogador: " + nomesJogadores[i]);
@@ -67,7 +127,7 @@ public class Main {
         System.out.println("2 - AUTOMÁTICO");
         System.out.println("Digite o número da opção desejada.");
     }
-    private static int[] gerarCartelaAutomatico(String[] nomesJogadores, int[][] cartelas) {
+    private static void gerarCartelaAutomatico(String[] nomesJogadores, int[][] cartelas) {
         int[] cartela = new int[tamanhoCartela];
 
         for (int i = 0; i <nomesJogadores.length; i++) {
@@ -81,12 +141,11 @@ public class Main {
                 cartelas[i][j] = cartela[j];
             }
         }
-        return cartela;
     }
-    private static int[][] gerarCartelaManual(String[] nomesJogadores, int[][] cartelas) {
+    private static void gerarCartelaManual(String[] nomesJogadores, int[][] cartelas) {
         int[] cartela = new int[tamanhoCartela];
         System.out.println("Então vamos cadastrar as cartelas do jogadores ... ");
-        System.out.printf("Você deve entrar com os %d números sorteados (entre 0 e 60), separados por vírgula e sem espaço." +
+        System.out.printf("Você deve entrar com os %d números da cartela (entre 0 e 60), separados por vírgula e sem espaço." +
                 "\nExemplo: 20,26,52,40,2" +
                 "\nOBS.: Os números não devem se repetir.\n\n", tamanhoCartela);
 
@@ -100,6 +159,5 @@ public class Main {
                 cartelas[i][j] = cartela[j];
             }
         }
-        return cartelas;
     }
 }
